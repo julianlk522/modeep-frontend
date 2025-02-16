c242a3e8a9097492a0e15e1b780eff3a8f090954
Julian Lindsay-Kaufman <101271303+julianlk522@users.noreply.github.com>
2025-02-16 02:06:37 -0500

    little fixes / edits

--- src/components/TmapProfile/Email.tsx
+++ src/components/TmapProfile/Email.tsx
@@ -29,9 +29,9 @@ export default function Email(props: Props) {
 			) : (
 				<strong className='profile-details-text'>not set</strong>
 			)}
-			<p class='profile-details-text'>
+			<p id='profile-email-note' class='profile-details-text'>
 				Only you can see your email. It is recommended to set one in
-				case you forget your password and need to send a reset link.
+				case you forget your password and need to send a recovery link.
 			</p>
 		</div>
 	)
